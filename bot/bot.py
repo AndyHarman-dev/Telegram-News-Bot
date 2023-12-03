ec0cb2d4cdc71f80a560d1f445e2b0b7d2c74209
Wiam <meganite647@gmail.com>
2023-12-03 22:00:44 +0400

    Fill in functions in TelegramBot with templated versions. Create a function for preferences validation that user inputs as a static method

--- bot/bot.py
+++ bot/bot.py
@@ -23,7 +23,7 @@ PLATFORMS_MESSAGE_TEXT = config.CONFIG_DICT['platforms_message']
 ACCOUNTS_MESSAGE_TEXT = config.CONFIG_DICT['accounts_message']
 
 # Define telegram bot states
-PLATFORMS, AUTHORIZE_PLATFORM, CHOOSE_GROUPS_ACCOUNTS, ENTITY, SET_PREFERENCES, SCHEDULE_GENERATION, GENERATE = range(7)
+PLATFORMS, AUTHORIZE_PLATFORM, CHOOSE_GROUPS_ACCOUNTS, ENTITY, EDIT_PREFERENCES, SCHEDULE_GENERATION, GENERATE = range(7)
 
 
 # Helper function for getting telegram api url
@@ -99,8 +99,8 @@ class TelegramBot:
                                          CallbackQueryHandler(self.handle_choose_groups_accounts_query)],
                 ENTITY: [MessageHandler(filters.TEXT, self.handle_entity,),
                          CallbackQueryHandler(self.handle_entity_query)],
-                SET_PREFERENCES: [MessageHandler(filters.TEXT, self.handle_set_preferences),
-                                  CallbackQueryHandler(self.handle_set_preferences_query)],
+                EDIT_PREFERENCES: [MessageHandler(filters.TEXT, self.handle_edit_preferences),
+                                   CallbackQueryHandler(self.handle_edit_preferences_query)],
                 SCHEDULE_GENERATION: [MessageHandler(filters.TEXT, self.handle_schedule_generation),
                                       CallbackQueryHandler(self.handle_schedule_generation_query)],
                 GENERATE: [MessageHandler(filters.TEXT, self.handle_generate),
@@ -258,29 +258,129 @@ class TelegramBot:
         # Move to the next entity state
         return ENTITY
 
+    """
+    Handles an entity by retrieving its preferences and displaying them as a message. 
+    The function takes an `update` object of type `Update` and a `context` object of type `ContextTypes.DEFAULT_TYPE`.
+
+    Parameters:
+        update (Update): The update object containing the information about the incoming message.
+        context (ContextTypes.DEFAULT_TYPE): The context object containing the additional information for handling the update.
+
+    Returns:
+        None: This function does not return anything.
+    """
     async def handle_entity(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        entity = self.__current_user.platform_entities[self.__current_platform][self.__current_entity]
+        preferences = entity.preferences
+        # Show the entity's preferences as a message
+        await update.message.reply_text(f"Current preferences for {entity.entity_name}:\n"
+                                        f"Topics: {preferences.topics}\n"
+                                        f"Avoid topics: {preferences.avoid_topics}\n"
+                                        f"Frequency: {preferences.frequency}")
+        # Show buttons: "Edit preferences", "Schedule Generation", "Generate", "Delete"
+        keyboard = [
+            [
+                InlineKeyboardButton("Edit preferences", callback_data='edit_preferences'),
+                InlineKeyboardButton("Schedule Generation", callback_data='schedule_generation'),
+                InlineKeyboardButton("Generate", callback_data='generate'),
+                InlineKeyboardButton("Delete", callback_data='delete')
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+    """
+    Handle entity query.
+
+    Parameters:
+        update (Update): The update containing the callback query.
+        context (ContextTypes.DEFAULT_TYPE): The context object.
+
+    Returns:
+        None
+
+    Description:
+        This function handles the entity query. It takes an update object and a context object as 
+        parameters. The update object contains the callback query, while the context object provides 
+        additional context for the function. The function does not return anything.
+
+        The function starts by retrieving the callback query from the update object. It then calls the 
+        "answer" method on the query object to send an empty answer to the callback query. 
+
+        The function then checks the value of the "data" attribute of the callback query. If it is 
+        "edit_preferences", the function returns the constant "EDIT_PREFERENCES". If it is 
+        "schedule_generation", the function returns the constant "SCHEDULE_GENERATION". If it is 
+        "generate", the function returns the constant "GENERATE". If it is "delete", the function 
+        deletes the entity from the current user's platform_entities dictionary and returns the constant 
+        "CHOOSE_GROUPS_ACCOUNTS".
+    """
     async def handle_entity_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        query = update.callback_query
+        await query.answer()
+        if query.data == 'edit_preferences':
+            return EDIT_PREFERENCES
+        elif query.data == 'schedule_generation':
+            return SCHEDULE_GENERATION
+        elif query.data == 'generate':
+            return GENERATE
+        elif query.data == 'delete':
+            # Delete the entity and return to the previous state
+            del self.__current_user.platform_entities[self.__current_platform][self.__current_entity]
+            return CHOOSE_GROUPS_ACCOUNTS
 
-    async def handle_set_preferences(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+    async def handle_edit_preferences(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        # Ask the user for new preferences
+        await update.message.reply_text("Please enter the new preferences in the following format:\n"
+                                        "topics: topic1, topic2, ...\n"
+                                        "avoid_topics: avoid_topic1, avoid_topic2, ...\n"
+                                        "frequency: daily/weekly/monthly")
 
-    async def handle_set_preferences_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+    async def handle_edit_preferences_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        # Parse the user's input and update the preferences
+        preferences_text = update.message.text
+        # Parse the preferences_text and update the preferences for the current entity
+        # ...
+        # Return to the ENTITY state
+        return ENTITY
 
     async def handle_schedule_generation(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        # Ask the user for the desired schedule time
+        await update.message.reply_text("Please enter the desired schedule time in the format: HH:MM")
 
     async def handle_schedule_generation_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        # Parse the user's input and schedule the generation
+        schedule_time_text = update.message.text
+        # Schedule the generation based on the schedule_time_text and the current entity's preferences
+        # ...
+        # Return to the ENTITY state
+        return ENTITY
 
     async def handle_generate(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        # Generate a sample post based on the current entity's preferences
+        sample_post = "Sample post content"  # Replace this with the actual generated content
+        await update.message.reply_text(f"Generated sample post:\n{sample_post}")
+
+        # Show buttons: "<< BACK" and "Post"
+        keyboard = [
+            [
+                InlineKeyboardButton("<< BACK", callback_data='back'),
+                InlineKeyboardButton("Post", callback_data='post')
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
     async def handle_generate_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        pass
+        query = update.callback_query
+        await query.answer()
+
+        if query.data == 'back':
+            return ENTITY
+        elif query.data == 'post':
+            # Post the generated content to the selected entity
+            # ...
+            await update.message.reply_text("The post has been published.")
+            return ENTITY
 
     async def handle_cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         pass
--- database/users.py
+++ database/users.py
@@ -1,5 +1,5 @@
 import json
-import entity
+import re
 
 
 # User preferences class realises a user preferences object to save it in database
@@ -19,6 +19,42 @@ class Preferences:
         self.topics = topics
         self.avoid_topics = avoid_topics
         self.frequency = frequency
+
+    @staticmethod
+    def validate_preferences(input_string):
+        # Split the input string into lines
+        lines = input_string.split("\n")
+
+        # Check if there are exactly three lines
+        if len(lines) != 3:
+            return False
+
+        # Validate the topics line
+        topics_line = lines[0]
+        if not topics_line.startswith("topics:"):
+            return False
+        topics = topics_line[len("topics:"):].split(",")
+        if not all(re.match(r"^\s*\w+\s*$", topic) for topic in topics):
+            return False
+
+        # Validate the avoid_topics line
+        avoid_topics_line = lines[1]
+        if not avoid_topics_line.startswith("avoid_topics:"):
+            return False
+        avoid_topics = avoid_topics_line[len("avoid_topics:"):].split(",")
+        if not all(re.match(r"^\s*\w+\s*$", avoid_topic) for avoid_topic in avoid_topics):
+            return False
+
+        # Validate the frequency line
+        frequency_line = lines[2]
+        if not frequency_line.startswith("frequency:"):
+            return False
+        frequency = frequency_line[len("frequency:"):].strip()
+        if frequency not in ["daily", "weekly", "monthly"]:
+            return False
+
+        # If all checks passed, the input is valid
+        return True
 
     def to_json(self):
         return json.dumps({
